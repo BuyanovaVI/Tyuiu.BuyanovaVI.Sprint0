bacba3bec2ea97d4836ecb271f4149dbb30ab4b5
Tyuiu.BuynovaVI@outlook.com <v.buyanova1@mail.ru>
2024-09-15 18:40:22 +0500

    Task3

---
+++ Tyuiu.BuyanovaVI.Sprint0.Task3.V0.Lib/DataService.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Tyuiu.BuyanovaVI.Sprint0.Task3.V0.Lib
+{
+    public class DataService
+    {
+        //Реализация методов Sum
+
+        public static int Sum(int a, int b)
+        { 
+            return a + b; 
+        }
+    }
+}
