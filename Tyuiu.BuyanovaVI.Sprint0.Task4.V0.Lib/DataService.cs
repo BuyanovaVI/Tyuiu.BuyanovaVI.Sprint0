36411540cc2fdc9c9aca696ec24b978f33aee06b
Tyuiu.BuynovaVI@outlook.com <v.buyanova1@mail.ru>
2024-09-24 23:48:26 +0500

    Task4

---
+++ Tyuiu.BuyanovaVI.Sprint0.Task4.V0.Lib/DataService.cs
@@ -0,0 +1,30 @@
+﻿namespace Tyuiu.BuyanovaVI.Sprint0.Task4.V0.Lib
+{
+    public class DataService
+    {
+        //Пример лин-й структ-ы
+
+        public static int Addition(int a, int b)
+            { 
+                return a + b; 
+            }
+
+        //Пример линейной структуры
+        public static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        //Пример линейной структуры
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        //Пример линейной структуры
+        public static double Division(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ Tyuiu.BuyanovaVi.Sprint0.Task4.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.BuyanovaVi.Sprint0.Task4.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
